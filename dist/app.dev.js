6a147e7be26ac0f5d7958189a1a1c9cff72f1214
JesseFervent <akohgodwinjesse@gmail.com>
2020-12-19 23:47:04 +0100

    Additives

---
+++ dist/app.dev.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var btn = document.querySelectorAll(".drum");
+var noOfBtn = btn.length;
+
+for (i = 0; i < 7; i++) {
+  btn[i].addEventListener("click", function () {
+    var btnInner = this.innerHTML;
+    makeSound(btnInner);
+  });
+}
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log("Wrong keyword entered");
+  }
+}
